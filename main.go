03bf8d48b6d1610460f43bb822e1356055664ec6
furkan caglar <furkan.caglar.34@gmail.com>
2020-06-19 00:04:03 +0700

    add some logging to show off

--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"log"
 	"math/rand"
+	"runtime"
 	"time"
 )
 
 func main() {
+	log.Printf("App Starting..\nCurrent total goroutine number=%v\n", runtime.NumGoroutine())
 
 	run()
+
+	// because garbage collector might not run for the last goroutine
+	// (in this case the last goroutine will not be removed)
+	// so logging will not work correctly
+	runtime.GC()
+
+	log.Printf("Sutting Down..\nCurrent total goroutine number=%v\n", runtime.NumGoroutine())
 
 }
 
 // application configs
 const (
-	NUMBER_OF_GOROUTINES = 5
+	NUMBER_OF_GOROUTINES = 7
 	NUMBER_OF_JOBS       = 10
 )
 
@@ -23,17 +33,20 @@ const (
 )
 
 type (
-	// send all of the go routines to let them know there is no more
+	// send all of the goroutines to let them know there is no more
 	// job coming so they shut themselves down.
-	// that means each go routine will read from this channel exactly once
-	// so a basic for loop will be enough to wait for all go routines to
+	// that means each goroutine will read from this channel exactly once
+	// so a basic for loop will be enough to wait for all goroutines to
 	// finish their last job.
 	signalEndOfJobs struct{}
 )
 
 var (
-	chnInteger         chan int32
-	chnSignalEndOfJobs chan signalEndOfJobs
+	chnInteger              chan int32
+	chnSignalEndOfJobs      chan signalEndOfJobs
+
+	// FOR LOGGING
+	goRoutineCounter = NUMBER_OF_GOROUTINES
 )
 
 func run() {
@@ -45,7 +58,11 @@ func run() {
 
 	initGoRoutines()
 
+	log.Printf("%v goroutine ready for executing jobs..\nCurrent total goroutine number=%v\n", NUMBER_OF_GOROUTINES, runtime.NumGoroutine())
+
 	writeNumbers()
+
+	log.Printf("END of jobs: all jobs are channeled into executer gorouines..\nCurrent total goroutine number=%v\n", runtime.NumGoroutine())
 
 	waitForLastGoRoutine()
 }
@@ -78,6 +95,8 @@ func execute() {
 			}
 			time.Sleep(time.Millisecond * time.Duration(i))
 		case <-chnSignalEndOfJobs:
+			log.Printf("number %v goroutine is shutting down..\nCurrent total goroutine number=%v", goRoutineCounter, runtime.NumGoroutine())
+			goRoutineCounter--
 			return
 		}
 	}
